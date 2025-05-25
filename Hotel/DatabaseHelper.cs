e3712e4f29a5584cde54d549615b8233a868cdb7
Sara Shoulan <sarashoulan10@gmail.com>
2025-05-26 01:26:45 +0300

    طبعًا للعلم هذا المشوع تم بناؤه سويًا بين أعضاء المجموعة، ضمن تجهيزنا لمناقشة العملي، لذلك لم يتم رفع التعديلات الأولية والملف من الصفر.

--- Hotel/DatabaseHelper.cs
+++ Hotel/DatabaseHelper.cs
@@ -7,7 +7,7 @@ public static class DatabaseHelper
 {
     private static string connectionString = "server=localhost;user=root;password=;database=hotel_db1";
 
-    // تنفيذ أوامر SELECT وترجيع DataTable مع دعم الباراميترات
+    // تنفيذ أوامر SELECT وترجيع ataTable مع دعم الباراميترات
     public static DataTable ExecuteSelectCommand(string query, List<MySqlParameter> parameters = null)
     {
         using (MySqlConnection con = new MySqlConnection(connectionString))
